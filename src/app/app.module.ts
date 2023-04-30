d5fc0e823f0cc89e895ad99d2f162cab629f4e3f
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2023-04-30 16:21:26 +0300

    2 56

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [ 
+  imports: [
+    BrowserModule,
     FormsModule,
-    BrowserModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
