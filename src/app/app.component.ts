694153b339cc7a567e5a54686526adeb52454687
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2023-04-30 16:14:10 +0300

    1 56

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,15 @@
-import { Component, ViewChild, ViewContainerRef, ComponentRef } from '@angular/core';
-import { PopUpComponent } from './pop-up/pop-up.component';
-import { UsersService } from './service/users.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [UsersService],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  users: any[];
+  title = 'home-work';
+submit(form: any) {
+    console.log(form);
 
-  constructor(public usersService:UsersService) {
-    this.users = this.usersService.getUsers();
   };
-  @ViewChild('popUp', { read: ViewContainerRef })
-  private viewRef!: ViewContainerRef;
-  private componentRef!: ComponentRef<PopUpComponent>;
-
-  showPopUp() {
-    this.componentRef = this.viewRef.createComponent(PopUpComponent);
-    this.componentRef.instance.name = this.usersService.group;
-    // this.componentRef.instance.members = this.users.length;
-   this.componentRef.instance.close.subscribe(() => { this.viewRef.clear()});
-  };
- change(){
-this.componentRef.instance.name=this.usersService.group
- }
-};
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { HeaderComponent } from './header/header.component';
-import { PopUpComponent } from './pop-up/pop-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserComponent,
-    HeaderComponent,
-    PopUpComponent
+    AppComponent
   ],
-  imports: [
+  imports: [ 
+    FormsModule,
     BrowserModule
   ],
   providers: [],
